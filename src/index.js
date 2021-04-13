19969211f7f7f30e53466dffd90e0fdbb3127e04
Ellen Nimako <ypokua@yahoo.com>
2021-04-13 23:26:12 +0100

    files updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
+function formateDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0$(hours)`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0$(minutes)`;
+  }
+
+  let days = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+  ];
+
+  let day = days[date.getDay()];
+  return `${day} ${hours} ${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let description = document.querySelector("#description");
+  let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formateDate(response.data.dt * 1000);
 }
 
 let apiKey = "15ebfc2dd8844ab614a089875bddc59e";
-let apiUrl = `htpps://api.openweathermap.org/data/2.5/weather?q=
-Edinburgh,uk&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/
+weather?q=${city}=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
