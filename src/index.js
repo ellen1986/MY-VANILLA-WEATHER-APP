05053baa314825e5374b4efca5ca4a6b808cd418
Ellen Nimako <ypokua@yahoo.com>
2021-04-15 17:32:41 +0100

    A new function and api url created in JS

--- src/index.js
+++ src/index.js
@@ -49,11 +49,11 @@ function displayForecast(response) {
   }
 
   function getForecast(coordinates) {
-  console.log(coordinates);
-   let apiKey = "15ebfc2dd8844ab614a089875bddc59e"
-   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-   console.log(apiUrl);
-   axios.get(apiUrl).then(displayForecast);
+    console.log(coordinates);
+    let apiKey = "15ebfc2dd8844ab614a089875bddc59e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
   }
 
 function displayTemperature(response) {
