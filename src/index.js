fa44837bdf6d32a3054ef4642461976cb65510cf
Ellen Nimako <ypokua@yahoo.com>
2021-04-16 15:32:15 +0100

    Consoles Removed from JS

--- src/index.js
+++ src/index.js
@@ -68,10 +68,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "15ebfc2dd8844ab614a089875bddc59e";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  
   axios.get(apiUrl).then(displayForecast);
 }
 
